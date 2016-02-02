b12e22e7d5820b13198db690acc9f47f278d7d3c
Roy Davis <myroy1024@yahoo.com>
2016-02-02 11:14:01 -0500

    Tic Tac Toe project

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,13 @@
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])         #prints the board
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+printBoard(theBoard)
     # TO DO #################################################################
     # Write code in this function that prints the game board.               #
     # The code in this function should only print, the user should NOT      #
@@ -6,9 +15,19 @@ def printBoard(board):
     #                                                                       #
     # Hint: you can follow the same process that was done in the textbook.  #
     #########################################################################
+  
 
 def checkWinner(board, player):    
     print('Checking if ' + player + ' is a winner...')
+    return ((board['top-L'] == player and board['top-M'] == player and board['top-R'] == player) or #checking across top row
+            (board['mid-L'] == player and board['mid-M'] == player and board['mid-R'] == player) or #checking across middle row
+            (board['low-L'] == player and board['low-M'] == player and board['low-R'] == player) or #checking across bottom row
+            (board['top-L'] == player and board['mid-L'] == player and board['low-L'] == player) or #checking vertically on left row
+            (board['top-M'] == player and board['mid-M'] == player and board['low-M'] == player) or #checking vertically the middle row
+            (board['top-R'] == player and board['mid-R'] == player and board['low-R'] == player) or #checking vertically the right row
+            (board['top-L'] == player and board['mid-M'] == player and board['low-R'] == player) or #checking diagonally
+            (board['top-R'] == player and board['mid-M'] == player and board['low-L'] == player))   #checking diagonally
+
     
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
@@ -28,18 +47,18 @@ def startGame(startingPlayer, board):
 
     turn = startingPlayer
     for i in range(9):
-        printBoard(board)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = input()
+        printBoard(board)                                       #prints the board before every turn
+        print('Turn for ' + turn + '. Move on which space?')    #asks player which space they choose
+        move = input()                                          #players choice on which space
         board[move] = turn
-        if( checkWinner(board, 'X') ):
+        if( checkWinner(board, 'X') ):                          #checking to see if X wins
             print('X wins!')
             break
-        elif ( checkWinner(board, 'O') ):
+        elif ( checkWinner(board, 'O') ):                       #checking to see if O wins
             print('O wins!')
             break
     
-        if turn == 'X':
+        if turn == 'X':                                         #if no winner the game continues
             turn = 'O'
         else:
             turn = 'X'
